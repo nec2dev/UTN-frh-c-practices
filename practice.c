4e56e5d26c478009aba9068483f8697433d7cb88
nec2dev <nec2multivackergithub@outlook.com>
2021-04-10 09:33:17 -0300

    varibles

---
+++ practice.c
@@ -0,0 +1,29 @@
+//Es lo mismo A = B que B = A?
+
+//Evaluar el siguiente conjunto de asignaciones:
+
+int   A , B , C , D , E , F ;
+
+A = 5 ;
+printf(A);
+
+B = 3 ;
+printf(B);
+
+C = (A+B)*2 ;
+printf(C);
+
+D = A+B*2 ;
+printf(D);
+
+E = A ;
+printf(E);
+
+F = A ;
+printf(F);
+
+A = B ;
+printf(A);
+
+B = F ;
+printf(B);
