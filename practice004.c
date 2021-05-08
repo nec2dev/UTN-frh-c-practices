c2c2e638172377b55d1ec0e67be3498fef0c0f7c
nec2dev <nec2multivackergithub@outlook.com>
2021-05-08 09:04:55 -0300

    ok

---
+++ practice004.c
@@ -0,0 +1,23 @@
+/*4. dada una cantidad de segundos, conviertirlas en horas, 
+minutos y segundos.*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+    long int segundos, horas;
+    int minutos, segundosfinal;
+   
+    printf("Por favor, Ingrese la cantidad de segundos\n");
+    scanf("%i", &segundos);
+
+    horas = segundos/3600;
+    //minutos = (segundos-horas*3600)/60;
+    minutos = (segundos%3600)/60;
+    segundosfinal = (segundos%3600)%60;
+
+    printf("%i seg. expresados en horas, minutos y segundos= %i:%i:%i.", segundos, horas, minutos, segundosfinal);
+
+   return 0;
+};  
