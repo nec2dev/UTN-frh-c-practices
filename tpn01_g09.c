16b56b63ee829ec6b06d24d78850ce181422cc41
nec2dev <nec2multivackergithub@outlook.com>
2021-05-22 10:57:35 -0300

    enunciado

--- tpn01_g09.c
+++ tpn01_g09.c
@@ -6,7 +6,19 @@ Al acceder al sistema el usuario debe ingresar:
     *Número de matrícula
     *Distancia del vuel a realizar
     *Cantidad de pasajeros
-A*/
+Aquellos valores ingresados erróneamente deben ser in-
+formados por pantalla. A saber:
+    *Número de matrícula:
+    entre 10000 y 20000 corresponde a aviones tipo Cessna
+    entre 20001 y 30000 corresponde a aviones tipo Boeing
+    entre 30001 y 40000 corresponden a aviones tipo Airbus
+    Cualquier otro Número de Matricula se considera erróneo, 
+    debe ser informado por pantalla y descartarse.
+El programa debe calcular e informar: El costo que genera 
+cada avión, sabiendo que el consumo promedio para cada tipo 
+de avión es: 
+        Cessna	        Boeing	        Airbus
+        0.535 US$/Km	49.665 US$/Km	10.464 US$/Km*/
 
 #include <stdio.h>
 
