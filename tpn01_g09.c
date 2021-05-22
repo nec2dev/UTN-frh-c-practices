7869e7f3b7e7cb18f5078a02f688a65fe43695d7
nec2dev <nec2multivackergithub@outlook.com>
2021-05-22 10:47:26 -0300

    first code

---
+++ tpn01_g09.c
@@ -0,0 +1,18 @@
+/*Les solicitan desarrollar un programa en lenguaje C para
+la evaluación de los costos generados por los aviones per-
+tenecientes a una campaña de chárter
+Etapa 1: Estructura secuencial y Estructura de desición
+Al acceder al sistema el usuario debe ingresar:
+    *Número de matrícula
+    *Distancia del vuel a realizar
+    *Cantidad de pasajeros
+A*/
+
+#include <stdio.h>
+
+int main() {
+
+    
+    
+    return 0;
+}; 
